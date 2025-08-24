c2d694d481cafa02540dd882be3c917a40854826
Tursunpolot <@gmail.com>
2025-08-24 15:38:15 +0500

    oooo

--- config.py
+++ config.py
@@ -5,22 +5,29 @@
 
 
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # .env faylni yuklaydi
+MONGODB_URI = os.getenv("MONGO_PUBLIC_URL")
+API_TOKEN = os.getenv("API_TOKEN")
 
 
-from environs import Env
 
-# Env obyektini yaratamiz
-env = Env()
-env.read_env()  # .env faylni yuklaydi
 
-# MongoDB sozlamalari
-MONGODB_URI = env.str("MONGO_PUBLIC_URL", "mongodb://mongo:wSUPTtKQyhVoPQaAScUxZoEwKAcoWgKc@gondola.proxy.rlwy.net:40669")
 
-# Bot sozlamalari
-BOT_TOKEN = env.str("BOT_TOKEN", "")
-ALLOWED_USER_IDS = env.list("ALLOWED_USER_IDS", subcast=int, default=[])
 
-# API va boshqa sozlamalar
-API_TOKEN = env.str("API_TOKEN", "MY_SECURETY_TOKEN")
-NUM_TABLES = env.int("NUM_TABLES", 7)
-HOURLY_RATE = env.int("HOURLY_RATE", 35000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
